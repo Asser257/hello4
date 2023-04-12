aab84a39604f48ae049a7d9bb5a08ba6ff6c6f6c
Asser257 <aatwan257@gmail.com>
2023-04-12 21:41:43 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const navEl = document.getElementById('navbar');
+window.addEventListener('scroll' , () => {
+
+    if(scrollY > 56){
+        navEl.classList.add('navbar-scrolled');
+    }
+    else if(scrollY < 56){
+navEl.classList.remove('navbar-scrolled')
+    }
+
+
+})
